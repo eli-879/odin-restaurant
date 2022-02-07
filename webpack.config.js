641cd1c7565f574588b40cbe07c983cfb3c5a504
Eric Li <ericxli2001@gmail.com>
2022-02-07 14:48:55 +1300

    changed to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: './dist',
   },
 };
